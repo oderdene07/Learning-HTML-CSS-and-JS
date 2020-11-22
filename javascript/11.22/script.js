7f11b5a67b820f85471b136735d90de9174afd8b
odko2000 <69565961+odko2000@users.noreply.github.com>
2020-11-22 17:59:38 +0800

    js

---
+++ javascript/11.22/script.js
@@ -0,0 +1,17 @@
+var a = 20;
+var b = 15;
+var P = 2 * (a + b);
+var S = a * b;
+console.log('tegsh untsugtiin urt = ' + a);
+console.log('tegsh untsugtiin urgun = ' + b);
+console.log('tegsh untsugtiin perimeter = ' + P);
+console.log('tegsh untsugtiin talbai = ' + S);
+
+
+
+var x = 32;
+var y = 45;
+var z = 35; 
+var p = x + y + z;
+console.log('gurvaljnii taluud: ' + x + ', ' + y + ', ' + z);
+console.log('gurvaljnii perimeter = ' + p);
