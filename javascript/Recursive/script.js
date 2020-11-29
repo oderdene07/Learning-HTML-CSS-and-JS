9225faaa8a241461d5ea66ad0fcc69e7599d3600
odko2000 <69565961+odko2000@users.noreply.github.com>
2020-11-29 23:42:40 +0800

    hhe

--- javascript/Recursive/script.js
+++ javascript/Recursive/script.js
@@ -1,17 +1,18 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-var sondgoi = []
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var sondgoi = [];
 
 function sondgoig_oloh(numbers){
     if (0 < numbers.length){
         if(numbers[0] % 2 === 1){
-            sondgoi.push(numbers[0])
-            numbers.shift()
-            sondgoig_oloh(numbers)
+            sondgoi.push(numbers[0]);
+            numbers.shift();
         }
         else
-            numbers.shift()
-            sondgoig_oloh(numbers)
-        }
+            numbers.shift();
+
+        sondgoig_oloh(numbers);
+        
+    }
 }
 sondgoig_oloh(numbers);
-console.log('Sondgoi toonuud: ' + sondgoi)
+console.log('Sondgoi toonuud: ' + sondgoi);
