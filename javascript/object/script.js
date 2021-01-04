f9da09011efafd8f22c51df4c1dd66bc7c89f40f
odko2000 <69565961+odko2000@users.noreply.github.com>
2021-01-04 15:43:59 +0800

    web bie daalt

---
+++ javascript/object/script.js
@@ -0,0 +1,32 @@
+var aav = {
+    Ovog: "Lkhagva",
+    Ner: "Natsagdorj",
+    Nas: 57,
+    Mergjil: "Emnelgiin tonog tuhuurumjiin injener"
+}
+var eej = {
+    Ovog: "Maam",
+    Ner: "Nyamsuren",
+    Nas: 56,
+    Mergjil: "Emnelgiin tonog tuhuurumjiin injener"
+}
+var egch = {
+    Ovog: "Natsagdorj",
+    Ner: "Odontuya",
+    Nas: 36,
+    Mergjil: "Ersdeliin shinjeech"
+}
+var ah = {
+    Ovog: "Natsagdorj",
+    Ner: "Odbayar",
+    Nas: 30,
+    Mergjil: "Ersdeliin shinjeech"
+}
+var bi = {
+    Ovog: "Natsagdorj",
+    Ner: "Od-Erdene",
+    Nas: 20,
+    Mergjil: "Oyutan"
+}
+var familyMembers = [aav, eej, egch, ah, bi];
+console.log(familyMembers);
