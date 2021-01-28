dd58fda80db0399225d9050bb4be7d50cd3cf29e
odko2000 <69565961+odko2000@users.noreply.github.com>
2021-01-28 17:33:45 +0800

    about me web

--- javascript/classList/script.js
+++ javascript/classList/script.js
@@ -1,17 +1,17 @@
-var text = document.querySelectorAll('.text');
-text.forEach(function(texts){
-    texts.onclick = function(){
-        if(texts.className.includes('orange')){
-            texts.nextElementSibling.classList.toggle('active');
-        }
-        if(texts.className.includes('purple')){
-            texts.nextElementSibling.classList.toggle('active');
-        }
-        if(texts.className.includes('green')){
-            texts.nextElementSibling.classList.toggle('active');
-        }
-        if(texts.className.includes('blue')){
-            texts.nextElementSibling.classList.toggle('active');
-        }
-      };
-})
+var text = document.querySelectorAll(".text");
+text.forEach(function (texts) {
+  texts.onclick = function () {
+    if (texts.className.includes("orange")) {
+      texts.nextElementSibling.classList.toggle("active");
+    }
+    if (texts.className.includes("purple")) {
+      texts.nextElementSibling.classList.toggle("active");
+    }
+    if (texts.className.includes("green")) {
+      texts.nextElementSibling.classList.toggle("active");
+    }
+    if (texts.className.includes("blue")) {
+      texts.nextElementSibling.classList.toggle("active");
+    }
+  };
+});
